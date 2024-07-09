efc5474d5429449d73009698961eb5b2bbf1a61b
Alice Bedow <alice.bedow@adesso.de>
2024-07-09 15:39:21 +0200

    Spring Boot Camunda Payment Application

---
+++ camunda8-getting-starting-with-microservice-orchestration/spring-c8-getting-started-microservice-orchestration-lab/spring-c8-getting-started-microservice-orchestration-lab/src/test/java/com/camunda/academy/spring/SpringPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camunda.academy.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
